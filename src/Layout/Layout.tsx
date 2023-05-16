81ea72cdbb0de52b75551d0525723afb2f05f291
Franceschio <coppolaciccio1@gmail.com>
2023-05-16 11:02:59 +0200

    Navbar layout

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,11 +1,12 @@
 import styles from "./index.module.scss";
-
 import Header from "../components/header";
+import Navbar from "../components/navbar";
 
 const Layout = ({ children }: any) => {
   return (
     <div className={styles.Layout}>
       <Header />
+      <Navbar />
       {children}
     </div>
   );
