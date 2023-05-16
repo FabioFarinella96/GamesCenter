5a6f40221c0aea23655bf76b5f2fda0289e0bd82
Franceschio <coppolaciccio1@gmail.com>
2023-05-16 10:59:44 +0200

    debug

--- src/components/componenteProva/Prova.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./index.module.scss";
-
-const Prova = (): any => {
-  return <div className={styles.Prova}>Ciao</div>;
-};
-
-export default Prova;
---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,7 @@
+import styles from "./index.module.scss";
+
+const Navbar = (): any => {
+  return <div className={styles.Navbar}></div>;
+};
+
+export default Navbar;
